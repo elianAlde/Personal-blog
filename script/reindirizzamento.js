f0f00e7d452f5b344bb85e0135428a4c5a2bbd9c
Elian Alderuccio <117550137+elianAlde@users.noreply.github.com>
2023-11-28 23:48:14 +0100

    Add files via upload

---
+++ script/reindirizzamento.js
@@ -0,0 +1,24 @@
+function apriDev(){
+    window.location.href = "menu/dev.html";
+}
+
+function apriTech(){
+    window.location.href = "menu/tech.html";
+}
+
+function apriDaily(){
+    window.location.href = "menu/daily.html";
+}
+
+function apriTravel(){
+    window.location.href = "menu/travel.html";
+}
+
+function apriMoto(){
+    window.location.href = "menu/moto.html";
+}
+
+function apriClimb(){
+    alert("Sezione in costruzione");
+    //window.location.href = "menu/climb.html";
+}
